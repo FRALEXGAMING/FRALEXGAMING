3755d43b487a939d2e6aacca1034287ce501d4a5
FRALEXGAMING <alexander.pichler.fi@gmail.com>
2016-12-26 09:45:20 +0100

    UPDATED SSH

--- TestDev/src/at/fralexgaming/Main.java
+++ TestDev/src/at/fralexgaming/Main.java
@@ -4,16 +4,6 @@ public class Main {
 
 	public static void main(String[] args) throws InterruptedException {
 
-
-		for(int faktor = 1; faktor <= 9; faktor ++ ) {
-			SSHConnect.sender("gpio write 1 1");
-			SSHConnect.sender("gpio write 1 0");
-
-		try{
-			Thread.sleep(250);
-		}catch(InterruptedException e){}
-		
+		SSHConnect.main("pi", "raspberrypi", "root", "gpio write 1 1");
 	}
-
-}
 }
--- TestDev/src/at/fralexgaming/SSHConnect.java
+++ TestDev/src/at/fralexgaming/SSHConnect.java
@@ -1,102 +1,38 @@
+
 package at.fralexgaming;
 
-import java.io.InputStream;
-import com.jcraft.jsch.Channel;
-import com.jcraft.jsch.ChannelExec;
-import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.Session;
+import com.jcraft.jsch.*;
 
 public class SSHConnect {
-	Session session = null;
-	Channel channel = null;
-	String host = "";
-	String user = "";
-	String password = "";
-	String command = "" ;
-	JSch jsch = null;
-	java.util.Properties config = null;
 	
+	public static void main(String user, String hostname, String password, String command){
 	
-	
-	SSHConnect(String command) {
-		this.host = "10.0.0.12";
-		this.user = "pi";
-		this.password = "root";
-		this.command = command;
-		
-	
-}
-	public  Channel initializeSession() {
-		try {
-			config = new java.util.Properties();
-			config.put("StrictHostKeyChecking", "no");
-			jsch = new JSch();
-			session = jsch.getSession(user, host, 22);
-			session.setPassword(password);
-			session.setConfig(config);
-			session.connect();
-			System.out.println("Session Connected...");
-			return session.openChannel("exec");
-		} catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
+	JSch jsch = new JSch();
 
-	public  void executeCommand(String command) {
-		try {
-			if (channel != null) {
-				System.out.println("<<<<<<<<not null>>>>>>>>>>");
-				((ChannelExec) channel).setCommand(command);
-				channel.setInputStream(null);
-				((ChannelExec) channel).setErrStream(System.err);
-				InputStream in = channel.getInputStream();
-				channel.connect();
-				readServerOutput(channel, in);
-				//in.close();
-				//channel.disconnect();
-				//session.disconnect();
-				System.out.println("DONE");
-				// System.exit(0);
-			} else {
-				System.out.println("Error occured...");
-				System.exit(0);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	try
+	{
+		//connection
+	  Session session = jsch.getSession(user, hostname, 22);
+	  session.setConfig("StrictHostKeyChecking", "no");
+	  session.setPassword(password);
+	  session.connect();
+
+	  
+	  Channel channel = session.openChannel("exec");
+	  ((ChannelExec) channel).setCommand(command);
+	  channel.setInputStream(null);
+	  ((ChannelExec) channel).setErrStream(System.err);
+
+	  channel.connect();
+
+	  channel.disconnect();
+	  session.disconnect();
+	}
+	catch (Exception e)
+	{
+	  System.out.println(e.getMessage());
+	}
 	}
 
 	
-
-	public void readServerOutput(Channel channel, InputStream in) {
-		byte[] tmp = new byte[1024];
-		try {
-			while (true) {
-				while (in.available() > 0) {
-					int i = in.read(tmp, 0, 1024);
-					if (i < 0) {
-						break;
-					}
-					System.out.print(new String(tmp, 0, i));
-				}
-				if (channel.isClosed()) {
-					System.out.println("exit-status: " + channel.getExitStatus());
-					break;
-				}
-				try {
-					Thread.sleep(1000);
-				} catch (Exception ee) {
-
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	public static void sender(String command) {
-		SSHConnect sce = new SSHConnect(command);
-		sce.executeCommand(command);
-	}
 }
